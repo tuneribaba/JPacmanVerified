539130bcd30b0613a64885acfffc45519fb00cea
Babatunde Akinniyi <tundeakinniyi@gmail.com>
2016-02-18 20:09:26 -0800

    Some test method name changes based on class notes the fail() method also throws assertionError so had to fix that THe failing cases for withinBorder parameter tests

--- src/test/java/org/jpacman/test/framework/model/BoardTest.java
+++ src/test/java/org/jpacman/test/framework/model/BoardTest.java
@@ -1,5 +1,6 @@
 package org.jpacman.test.framework.model;
 
+import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.Collection;
@@ -7,8 +8,6 @@ import org.jpacman.framework.model.Board;
 import org.jpacman.framework.model.Ghost;
 import org.jpacman.framework.model.IBoardInspector.SpriteType;
 import org.jpacman.framework.model.Tile;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -17,49 +16,53 @@ public class BoardTest {
 
 	private static final int width = 10;
 	private static final int height = 10;
-	
 	private Board board;
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUpBoard() throws Exception {
 		board = new Board(width, height);
-		
 	}
 
 	@Test
-	public void testBoardTooWide() {
-
+	public void boardWidthNonPositive() {
+		boolean passed = false;
 		try {
-			new Board(1, -1);
-			fail();
+			new Board(-1, 1);
+			passed = true;
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
 		}
 	}
 	
 	@Test
-	public void testBoardTooTall() {
-
+	public void boardHeightNonPositive() {
+		boolean passed = false;
 		try {
-			new Board(-1, -1);
-			fail();
+			new Board(1, -1);
+			passed = true;
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);;
+			assertTrue(true);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
 		}		
 	}
 
 	@Test
-	public void testGetHeight() {
-		assertEquals(height, board.getHeight());;
+	public void getHeightValid() {
+		assertEquals(height, board.getHeight());
 	}
 	
 	@Test
-	public void testGetWidth() {
-		assertEquals(width, board.getWidth());;
+	public void getWidthValid() {
+		assertEquals(width, board.getWidth());
 	}
 	
 	@Test
-	public void testPut() {
+	public void putValid() {
 		int x = 5;
 		int y = 5;
 		
@@ -67,48 +70,67 @@ public class BoardTest {
 		board.put(ghost, x, y);
 		
 		assertTrue(board.tileAt(x, y).containsSprite(ghost));
-
 	}
 	
 	@Test
-	public void testPutNull() {
+	public void putInvalidNull() {
+		boolean passed = false;
 		try {
 			board.put(null, 0, 0);
-			fail();
+			passed = true;
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
 		}	
 	}
 	
 	@Test
-	public void testPutOutside() {
+	public void putInvalidTileOutside() {
 		Ghost ghost = new Ghost();
-
+		boolean passed = false;
+		
+		try {
+			board.put(ghost, 29, 20);
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
+		} catch (AssertionError e) { 
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
+		}		
+	}
+	
+	@Test
+	public void putInvalidTileNonPositive() {
+		Ghost ghost = new Ghost();
+		boolean passed = false;
+		
 		try {
 			board.put(ghost, -1, 0);
-			fail();
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);
-		}	
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
+		}		
 	}
 
 	@Test
-	public void testWithinBorders() {
+	public void withinBordersBoundaries() {
 
 		assertTrue(board.withinBorders(5, 5));
-		
 		assertTrue(board.withinBorders(0, 0));
 		assertTrue(board.withinBorders(0, 9));
 		assertTrue(board.withinBorders(9, 0));
-		assertTrue(board.withinBorders(9, 9));
-		
+		assertTrue(board.withinBorders(9, 9));		
 		assertFalse(board.withinBorders(0, 10));
 		assertFalse(board.withinBorders(-1, 0));
 	
 	}
 
 	@Test
-	public void testSpriteAt() {
+	public void spriteAtOnBoard() {
 		int x = 5;
 		int y = 5;
 		
@@ -119,7 +141,7 @@ public class BoardTest {
 	}
 
 	@Test
-	public void testSpriteTypeAt() {
+	public void spriteTypeAtOnBoard() {
 		int x = 5;
 		int y = 5;
 		
@@ -130,19 +152,23 @@ public class BoardTest {
 	}
 	
 	@Test
-	public void testSpriteTypeAtOutside() {
+	public void spriteTypeAtOutsideBoard() {
+		boolean passed = false;
 
 		try {
-			assertEquals(SpriteType.GHOST, board.spriteTypeAt(-1, 0));
-			fail();
+			assertEquals(SpriteType.EMPTY, board.spriteTypeAt(-1, 0));
+			passed = true;
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
 		}
 
 	}
 
 	@Test
-	public void testPositiveTileAt() {
+	public void tileAtValid() {
 		int x = 5;
 		int y = 5;
 		
@@ -152,18 +178,23 @@ public class BoardTest {
 	}
 	
 	@Test
-	public void testTileAtOutside() {
+	public void tileAtInvalidOutside() {
+		boolean passed = false;
 
 		try {
 			board.tileAt(0, -1);
-			fail();
+			passed = true;
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
 		}
 	}
 
 	@Test
-	public void testOnBoardMessageFail() {
+	public void onBoardMessageFail() {
+		boolean passed = false;
 
 		int x = -1;
 		int y = 5;
@@ -171,36 +202,48 @@ public class BoardTest {
 		Ghost ghost = new Ghost();
 		try {
 			board.put(ghost, x, y);
-			fail();
+			passed = true;
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
 		}
 	}
 	
 	@Test
-	public void testTileAtOffsetFail() {
+	public void tileAtOffsetFail() {
+		boolean passed = false;
+
 		int x = 5;
 		int y = 5;
 		
 		try {
 			board.tileAtOffset(null, x, y);
-			fail();
+			passed = true;
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
 		}
 	}
 	
 	@Test
-	public void testTileAtOffsetOutside() {
+	public void tileAtOffsetOutside() {
 		int x = 5;
 		int y = 5;
 		Tile tile = new Tile(11, 11);
+		boolean passed = false;
 		
 		try {
 			board.tileAtOffset(tile, x, y);
-			fail();
+			passed = true;
+			//fail("Shouldn't get here");  - fail() throws AssertionError too just like the anticipated AssertionError from class
 		} catch (AssertionError e) {
-			assertTrue(true);
+			assertFalse(passed);
+		} finally {
+			assertFalse("non-positive and exception not thrown", passed);
 		}
 	}
 	
@@ -221,8 +264,13 @@ public class BoardTest {
 		}
 		
 		@Test
-		public void testWithinBorder() {
-			assertTrue(board.withinBorders(x, y));
+		public void withinBorderValidAndInvalid() {
+			if (((x >= 0) && (x < 10)) && ((y >= 0) && (y < 10))) {
+				//within boundary
+				assertTrue(board.withinBorders(x, y));				
+			} else {
+				assertFalse(board.withinBorders(x, y));
+			}
 		}
 		
 		@Parameters
